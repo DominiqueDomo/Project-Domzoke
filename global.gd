c66cafe8c74012f168cd2b42e7f4e56de12d264b
Dominique <dominiquevording@gmail.com>
2025-02-28 17:31:18 +0100

    Dominic returns

--- global.gd
+++ global.gd
@@ -3,7 +3,9 @@ extends Node
 var text_array = []
 var anim_array = []
 var char_array = []
-var pos_array = []
+var posA_array = []
+var posB_array = []
+var posC_array = []
 
 var gatekeeping = false
 
