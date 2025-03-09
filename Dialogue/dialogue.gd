f5318bca7f93f7379f11fd08860aa70ac5fe4c52
Dominique <dominiquevording@gmail.com>
2025-03-09 12:31:11 +0100

    Beast slain: Now working on char display
    I HAVE SLAIN THE BEAST
    
    Big ugly hardcoded loop has been destroyed

--- Dialogue/dialogue.gd
+++ Dialogue/dialogue.gd
@@ -70,124 +70,15 @@ func setup_convo(convo):
 						if Conversation.has(string):  # Safely check if the key exists
 							var stringvalue = Conversation[string]
 							Global.text_array.push_back(stringvalue)
-							var howmanyvarsarethere = Global.allvars_array.size()
-							for p in range(1, howmanyvarsarethere):
+							#repeats this loop for every variable in the "allvars" array
+							for p in range(0, Global.allvars_array.size()):
+								#checks if this variable is present in the JSON file at position i
 								if Conversation.has(Global.allvars_array[p] + str(i)):
-									var pushvalue = Global.allvars_array[p] + "value"
-									var pusharray = Global.allvars_array[p] + "_array"
-									Global.pusharray.push_back(Global.Global.allvars_array[p])
-								else:
-									Global.pusharray.push_back("NONE")
-						# Pushes var charA
-							#if Conversation.has(charA):
-								#Global.charAvalue = Conversation[charA]
-								#var preloads = "res://Characters/" + Global.charAvalue + ".tscn"
-								#Global.charA_array.push_back(Global.charAvalue)
-							#else:
-								#Global.charA_array.push_back("NONE")
-#
-							#if Conversation.has(charB):
-								#Global.charBvalue = Conversation[charB]
-								#var preloads = "res://Characters/" + Global.charBvalue + ".tscn"
-								#Global.charB_array.push_back(Global.charBvalue)
-							#else:
-								#Global.charB_array.push_back("NONE")
-#
-							#if Conversation.has(charC):
-								#Global.charCvalue = Conversation[charC]
-								#var preloads = "res://Characters/" + Global.charCvalue + ".tscn"
-								#Global.charC_array.push_back(Global.charCvalue)
-							#else:
-								#Global.charC_array.push_back("NONE")
-#
-							#if Conversation.has(charD):
-								#Global.charDvalue = Conversation[charD]
-								#var preloads = "res://Characters/" + Global.charDvalue + ".tscn"
-								#Global.charD_array.push_back(Global.charDvalue)
-							#else:
-								#Global.charD_array.push_back("NONE")
-#
-							#if Conversation.has(animA):
-								#Global.animAvalue = Conversation[animA]
-								#Global.animA_array.push_back(Global.animAvalue)
-							#else:
-								#Global.animA_array.push_back("NONE")
-#
-							#if Conversation.has(animB):
-								#Global.animBvalue = Conversation[animB]
-								#Global.animB_array.push_back(Global.animBvalue)
-							#else:
-								#Global.animB_array.push_back("NONE")
-#
-							#if Conversation.has(animC):
-								#Global.animCvalue = Conversation[animC]
-								#Global.animC_array.push_back(Global.animCvalue)
-							#else:
-								#Global.animC_array.push_back("NONE")
-#
-							#if Conversation.has(animD):
-								#Global.animDvalue = Conversation[animD]
-								#Global.animD_array.push_back(Global.animDvalue)
-							#else:
-								#Global.animD_array.push_back("NONE")
-#
-							#if Conversation.has(oriA):
-								#Global.oriAvalue = Conversation[oriA]
-								#Global.oriA_array.push_back(Global.oriAvalue)
-							#else:
-								#Global.oriA_array.push_back("NONE")
-#
-							#if Conversation.has(oriB):
-								#Global.oriBvalue = Conversation[oriB]
-								#Global.oriB_array.push_back(Global.oriBvalue)
-							#else:
-								#Global.oriB_array.push_back("NONE")
-#
-							#if Conversation.has(oriC):
-								#Global.oriCvalue = Conversation[oriC]
-								#Global.oriC_array.push_back(Global.oriCvalue)
-							#else:
-								#Global.oriC_array.push_back("NONE")
-#
-							#if Conversation.has(oriD):
-								#Global.oriDvalue = Conversation[oriD]
-								#Global.oriD_array.push_back(Global.oriDvalue)
-							#else:
-								#Global.oriD_array.push_back("NONE")
-#
-							#if Conversation.has(posA):
-								#Global.posAvalue = Conversation[posA]
-								#Global.posA_array.push_back(Global.posAvalue)
-							#else:
-								#Global.posA_array.push_back("NONE")
-#
-							#if Conversation.has(posB):
-								#Global.posBvalue = Conversation[posB]
-								#Global.posB_array.push_back(Global.posBvalue)
-							#else:
-								#Global.posB_array.push_back("NONE")
-#
-							#if Conversation.has(posC):
-								#Global.posCvalue = Conversation[posC]
-								#Global.posC_array.push_back(Global.posCvalue)
-							#else:
-								#Global.posC_array.push_back("NONE")
-#
-							#if Conversation.has(posD):
-								#Global.posDvalue = Conversation[posD]
-								#Global.posD_array.push_back(Global.posDvalue)
-							#else:
-								#Global.posD_array.push_back("NONE")
-#
-							#if Conversation.has(yapper):
-								#Global.yapperValue = Conversation[yapper]
-								#Global.yapper_array.push_back(Global.yapperValue)
-							#else:
-								#Global.yapper_array.push_back("NONE")
-
-
-
-							
+									#if it is present, it will push back the value found at location i for variable var[p]
+									Global.get(Global.allvars_array[p] + "_array").push_back(Conversation[(Global.allvars_array[p] + str(i))])
+								else:	
+									#if it's not present, it will push "NONE" into the array instead
+									Global.get(Global.allvars_array[p] + "_array").push_back("NONE")
 							# read through the JSON file, and finds everything with valuex for example
 						else:
 							print("dude you're missing the var string")
@@ -212,40 +103,40 @@ func display_label_text():
 		var textspeed = length * 0.07
 
 		#Change position by replacing the entire vector  
-		#checks if the character exists
+		#checks if there's a character at this spot in the conversation
 		if Global.charA_array[text] != "NONE": 
-			#makes the character play their animation
 			#it checks if this is the first line of dialogue
 			if text != 0:
-				#if it's not the first line of dialogue, it checks if the previous character matches any of the current characters
+				#if it's not the first line of dialogue, it checks if the previous character matches the current character at that location
 				if get_node("../%s" % Global.charA_array[text]) == get_node("../%s" % Global.charA_array[text-1]):
-					#if they match, the character stays the same, and no one turns invisible
+					#if they match, the character stays the same, and no one turns invisible, and plays the animation
 					get_node("../%s" % Global.charA_array[text]).play(Global.animA_array[text])
 					#checks if a position is given, otherwise, puts it at the default position for charA
 					if Global.posA_array[text] == "NONE":
 						get_node("../%s" % Global.charA_array[text]).set_position(Vector2(250, 300))
 					else:
 						get_node("../%s" % Global.charA_array[text]).set_position(Vector2(Global.posA_array[text]))
-					#makes the character face either right or left
+					#makes the character face either right or left, depending on the default position
 					if Global.oriA_array[text] != "NONE":
-						get_node("../%s" % Global.charA_array[text]).set_scale(Global.Global.oriA_array[text])
+						get_node("../%s" % Global.charA_array[text]).set_scale(Global.get(Global.oriA_array[text]))
 					else:
 						get_node("../%s" % Global.charA_array[text]).set_scale(Vector2(10, 10))
 				else:
-					#if they don't match, the previous character gets turned invisible, and the current character becomes visible
-					get_node("../%s" % Global.charA_array[text]).visible = true
-					get_node("../%s" % Global.charA_array[text-1]).visible = false
-					get_node("../%s" % Global.charA_array[text]).play(Global.animA_array[text])
-					#checks if a position is given, otherwise, puts it at the default position for charA
-					if Global.posA_array[text] == "NONE":
-						get_node("../%s" % Global.charA_array[text]).set_position(Vector2(250, 300))
-					else:
-						get_node("../%s" % Global.charA_array[text]).set_position(Vector2(Global.posA_array[text]))
-					#makes the character face either right or left
-					if Global.oriA_array[text] != "NONE":
-						get_node("../%s" % Global.charA_array[text]).set_scale(Global.Global.oriA_array[text])
-					else:
-						get_node("../%s" % Global.charA_array[text]).set_scale(Vector2(10, 10))
+					if get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charA_array[text]) and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charB_array[text]) and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charC_array[text]) and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charD_array[text]):
+					#this if-case makes sure that the previous character doesnt match ANY of the current characters. So they only turn invisible if they're leaving field entirely
+						get_node("../%s" % Global.charA_array[text]).visible = true
+						get_node("../%s" % Global.charA_array[text-1]).visible = false
+						get_node("../%s" % Global.charA_array[text]).play(Global.animA_array[text])
+						#checks if a position is given, otherwise, puts it at the default position for charA
+						if Global.posA_array[text] == "NONE":
+							get_node("../%s" % Global.charA_array[text]).set_position(Vector2(250, 300))
+						else:
+							get_node("../%s" % Global.charA_array[text]).set_position(Vector2(Global.posA_array[text]))
+						#makes the character face either right or left
+						if Global.oriA_array[text] != "NONE":
+							get_node("../%s" % Global.charA_array[text]).set_scale(Global.Global.oriA_array[text])
+						else:
+							get_node("../%s" % Global.charA_array[text]).set_scale(Vector2(10, 10))
 			else:
 				#if it's not the first line of dialogue, then there's no previous character, so no one to turn invisible
 				get_node("../%s" % Global.charA_array[text]).visible = true
@@ -256,70 +147,77 @@ func display_label_text():
 				else:
 					get_node("../%s" % Global.charA_array[text]).set_position(Vector2(Global.posA_array[text]))
 				#makes the character face either right or left
-					if Global.oriA_array[text] != "NONE":
-						get_node("../%s" % Global.charA_array[text]).set_scale(Global.Global.oriA_array[text])
-					else:
-						get_node("../%s" % Global.charA_array[text]).set_scale(Vector2(10, 10))
+				if Global.oriA_array[text] != "NONE":
+					get_node("../%s" % Global.charA_array[text]).set_scale(Vector2(10, 10))
+				else:
+					get_node("../%s" % Global.charA_array[text]).set_scale(Global.Global.oriA_array[text])
+		else:
+			if text != 0 and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charA_array[text]) and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charB_array[text]) and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charC_array[text]) and get_node("../%s" % Global.charA_array[text-1]) != get_node("../%s" % Global.charD_array[text]):
+			#if there is no current character on this position, the previous character should disappear
+			#AND the previous character doesn't match ANY of the current characters
+				get_node("../%s" % Global.charA_array[text-1]).visible = false
+			
+				
 
-		if Global.charB_array[text] != "NONE": 
-			get_node("../%s" % Global.charB_array[text]).play(Global.animB_array[text])
-			print(Global.posB_array[text])
-			if Global.posB_array[text] == "NONE":
-				get_node("../%s" % Global.charB_array[text]).set_position(Vector2(900, 300))
-			else:
-				get_node("../%s" % Global.charB_array[text]).set_position(Vector2(Global.posB_array[text]))
-			if Global.oriB_array[text] == "right":
-				get_node("../%s" % Global.charB_array[text]).set_scale(Vector2(10, 10))
-			else:
-				get_node("../%s" % Global.charB_array[text]).set_scale(Vector2(-10, 10))
-#charC is broken right now
-		if Global.charC_array[text] != "NONE":
-			if text != 0 and Global.charC_array[text-1] != "NONE":
-				if Global.charC_array[text] == Global.charC_array[text-1]:
-					get_node("../%s" % Global.charC_array[text]).play(Global.animC_array[text])
-					if Global.posC_array[text] == "NONE":
-						get_node("../%s" % Global.charC_array[text]).set_position(Vector2(500, 300))
-					else:
-						get_node("../%s" % Global.charC_array[text]).set_position(Vector2(Global.posC_array[text]))
-					if Global.oriC_array[text] == "right":
-						get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(10, 10))
-					else:
-						get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(-10, 10))
-				else:
-					get_node("../%s" % Global.charC_array[text]).visible = true
-					get_node("../%s" % Global.charC_array[text-1]).visible = false
-					get_node("../%s" % Global.charC_array[text]).play(Global.animC_array[text])
-					if Global.posC_array[text] == "NONE":
-						get_node("../%s" % Global.charC_array[text]).set_position(Vector2(500, 300))
-					else:
-						get_node("../%s" % Global.charC_array[text]).set_position(Vector2(Global.posC_array[text]))
-					if Global.oriC_array[text] == "right":
-						get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(10, 10))
-					else:
-						get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(-10, 10))
-			else:
-				get_node("../%s" % Global.charC_array[text]).visible = true
-				get_node("../%s" % Global.charC_array[text]).play(Global.animC_array[text])
-				if Global.posC_array[text] == "NONE":
-					get_node("../%s" % Global.charC_array[text]).set_position(Vector2(500, 300))
-				else:
-					get_node("../%s" % Global.charC_array[text]).set_position(Vector2(Global.posC_array[text]))
-				if Global.oriC_array[text] == "right":
-					get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(10, 10))
-				else:
-					get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(-10, 10))
-
-		if Global.charD_array[text] != "NONE": 
-			get_node("../%s" % Global.charD_array[text]).play(Global.animD_array[text])
-			print(Global.posD_array[text])
-			if Global.posD_array[text] == "NONE":
-				get_node("../%s" % Global.charD_array[text]).set_position(Vector2(650, 300))
-			else:
-				get_node("../%s" % Global.charD_array[text]).set_position(Vector2(Global.posD_array[text]))
-			if Global.oriD_array[text] == "right":
-				get_node("../%s" % Global.charD_array[text]).set_scale(Vector2(10, 10))
-			else:
-				get_node("../%s" % Global.charD_array[text]).set_scale(Vector2(-10, 10))
+		#if Global.charB_array[text] != "NONE": 
+			#get_node("../%s" % Global.charB_array[text]).play(Global.animB_array[text])
+			#print(Global.posB_array[text])
+			#if Global.posB_array[text] == "NONE":
+				#get_node("../%s" % Global.charB_array[text]).set_position(Vector2(900, 300))
+			#else:
+				#get_node("../%s" % Global.charB_array[text]).set_position(Vector2(Global.posB_array[text]))
+			#if Global.oriB_array[text] == "right":
+				#get_node("../%s" % Global.charB_array[text]).set_scale(Vector2(10, 10))
+			#else:
+				#get_node("../%s" % Global.charB_array[text]).set_scale(Vector2(-10, 10))
+##charC is broken right now
+		#if Global.charC_array[text] != "NONE":
+			#if text != 0 and Global.charC_array[text-1] != "NONE":
+				#if Global.charC_array[text] == Global.charC_array[text-1]:
+					#get_node("../%s" % Global.charC_array[text]).play(Global.animC_array[text])
+					#if Global.posC_array[text] == "NONE":
+						#get_node("../%s" % Global.charC_array[text]).set_position(Vector2(500, 300))
+					#else:
+						#get_node("../%s" % Global.charC_array[text]).set_position(Vector2(Global.posC_array[text]))
+					#if Global.oriC_array[text] == "right":
+						#get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(10, 10))
+					#else:
+						#get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(-10, 10))
+				#else:
+					#get_node("../%s" % Global.charC_array[text]).visible = true
+					#get_node("../%s" % Global.charC_array[text-1]).visible = false
+					#get_node("../%s" % Global.charC_array[text]).play(Global.animC_array[text])
+					#if Global.posC_array[text] == "NONE":
+						#get_node("../%s" % Global.charC_array[text]).set_position(Vector2(500, 300))
+					#else:
+						#get_node("../%s" % Global.charC_array[text]).set_position(Vector2(Global.posC_array[text]))
+					#if Global.oriC_array[text] == "right":
+						#get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(10, 10))
+					#else:
+						#get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(-10, 10))
+			#else:
+				#get_node("../%s" % Global.charC_array[text]).visible = true
+				#get_node("../%s" % Global.charC_array[text]).play(Global.animC_array[text])
+				#if Global.posC_array[text] == "NONE":
+					#get_node("../%s" % Global.charC_array[text]).set_position(Vector2(500, 300))
+				#else:
+					#get_node("../%s" % Global.charC_array[text]).set_position(Vector2(Global.posC_array[text]))
+				#if Global.oriC_array[text] == "right":
+					#get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(10, 10))
+				#else:
+					#get_node("../%s" % Global.charC_array[text]).set_scale(Vector2(-10, 10))
+#
+		#if Global.charD_array[text] != "NONE": 
+			#get_node("../%s" % Global.charD_array[text]).play(Global.animD_array[text])
+			#print(Global.posD_array[text])
+			#if Global.posD_array[text] == "NONE":
+				#get_node("../%s" % Global.charD_array[text]).set_position(Vector2(650, 300))
+			#else:
+				#get_node("../%s" % Global.charD_array[text]).set_position(Vector2(Global.posD_array[text]))
+			#if Global.oriD_array[text] == "right":
+				#get_node("../%s" % Global.charD_array[text]).set_scale(Vector2(10, 10))
+			#else:
+				#get_node("../%s" % Global.charD_array[text]).set_scale(Vector2(-10, 10))
 
 		visible_text_tween = create_tween()
 		visible_text_tween.tween_property(dialogue, "visible_ratio", 1.0, textspeed)
