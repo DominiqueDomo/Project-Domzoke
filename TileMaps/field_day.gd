238771b4e69f799515785c18ba60e03f29a50698
RubbishCans <tomhofverberg@gmail.com>
2025-03-18 18:28:04 +0100

    fifth commit
    Remade the cloud
    
    Added detail to the sunrise background
    
    Change the text sizes

--- TileMaps/field_day.gd
+++ TileMaps/field_day.gd
@@ -1,10 +1,12 @@
 extends TileMapLayer
 
 @onready var sun = $Sun
+@onready var clouds = $Clouds
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	sun.play("default")
+	clouds.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
