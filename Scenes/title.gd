0550cbc477acf67f729c89b7b1414b063dec0876
RubbishCans <tomhofverberg@gmail.com>
2025-04-03 09:41:46 +0200

    third commit - new title and credits
    new title and credits scene

--- Scenes/title.gd
+++ Scenes/title.gd
@@ -1,10 +1,25 @@
 extends Node2D
 
+@onready var torch1 = $Torch1
+@onready var torch2 = $Torch2
+@onready var torch3 = $Torch3
+@onready var torch4 = $Torch4
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	torch1.play("unlit")
+	torch2.play("unlit")
+	torch3.play("unlit")
+	torch4.play("unlit")
+	
+	
+	await get_tree().create_timer(0.5).timeout
+	torch1.play("lit")
+	torch4.play("lit")
+	await get_tree().create_timer(0.5).timeout
+	torch2.play("lit")
+	torch3.play("lit")
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
