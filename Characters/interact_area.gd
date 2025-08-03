c68c87e1408fe8ecccc82afb94f5d03475637b2d
DominiqueDomo <dominiquevording@gmail.com>
2025-08-03 14:13:56 +0200

    Working on interaction

---
+++ Characters/interact_area.gd
@@ -0,0 +1,5 @@
+class_name Interactible extends Area2D
+
+@export var interact_label = "E" 
+@export var interact_type = "dialogue"
+@export var interact_value = "none"
--- Characters/player.gd
+++ Characters/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 var last_direction = Vector2(1, 0)
-const SPEED = 150.0
+const SPEED = 100.0
 
 func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
@@ -16,14 +16,22 @@ func _physics_process(delta):
 		play_idle_animation(last_direction)
 		
 func play_walk_animation(direction):
+	var posx
+	var posy
+	var scalex
+	var scaley
 	if direction.x > 0:
 		$AnimatedSprite2D.play("move_right")
+		posx = 11
+		posy = -6
 	elif direction.x < 0:
 		$AnimatedSprite2D.play("move_left")
 	elif direction.y > 0:
 		$AnimatedSprite2D.play("move_down")
 	elif direction.y < 0:
 		$AnimatedSprite2D.play("move_up")
+	#$interactHitbox/CollisionShape2D2.set_position(Vector2(posx, posy))
+	#$interactHitbox/CollisionShape2D2.set_scale(Vector2(scalex, scaley))
 
 func play_idle_animation(direction):
 	if direction.x > 0:
